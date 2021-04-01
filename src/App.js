f3862ebd652b9bdd2ee3f73e43cd8e0e9632b95d
eslazyk10 <eslazy32@gmail.com>
2021-04-01 12:50:45 -0500

    Organized css into separate files associated with js files, updated layout and styling, moved temperature.js to current.js file to improve layout and organization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 
 import './App.css';
 import Search from "./Search";
-import Temperature from "./Temperature";
 import Current from "./Current";
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
-        <Search />
-      <h1 className="Current-City">Nashville</h1>
       <div className="row">
         <div className="col-6">
+      <h1 className="Current-City">Nashville</h1>
           <h2 className="Current-Day-Time">Thursday</h2>
           <h2 className="Current-Condition">Sunny</h2>
-          <Temperature value={70} />
+          
         </div>
         <div className="col-6">
-          <Current value={72} humidity={45} speed={8} />
+          <Current temp={70} value={72} humidity={45} speed={8} />
         </div>
       </div>
+      <Search />
       </div>
       <footer className="Footer"> <a href="https://github.com/eslazyk10/weather-react-app/tree/master/src" target="blank">Open-sourced code</a><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="20px" alt="github"/> by Emily Slazyk </footer>
     </div>
--- src/Current.js
+++ src/Current.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import "./Current.css";
+
 export default function Current(props) {
   return (
     <div className="Current">
+      <h3>
+        <img
+          src="https://d29fhpw069ctt2.cloudfront.net/icon/image/49043/preview.svg"
+          alt="Sunny"
+        />
+        {props.temp}° <a href="/">F</a>|<a href="/">C</a>
+      </h3>
       <ul>
         <li>Feels like: {props.value}°</li>
         <li>Humidity: {props.humidity}%</li>
