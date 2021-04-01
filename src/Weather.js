d8ef304ce8edbb4952b0753f07238110dad19c44
eslazyk10 <eslazy32@gmail.com>
2021-04-01 15:48:26 -0500

    updated useState efficiency utilizing weather data object

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,8 @@ import "./Weather.css";
 
 export default function Weather(props) {
   let [city, setCity]= useState("");
-  let [currentCity, setCurrentCity] = useState("Nashville");
-  let [temperature, setTemperature] = useState("70");
-  let [feel, setFeel] = useState("72");
-  let [humidity, setHumidity] = useState("52");
-  let [windSpeed, setWindSpeed] = useState("8");
+  let [weatherData, setWeatherData] = useState({});
+  let iconUrl ="https://d29fhpw069ctt2.cloudfront.net/icon/image/49043/preview.svg";
   
   const apiKey = "5b8bfd096caf5847f3506db76bfb75ad";
   const unit= "imperial";
@@ -27,12 +24,14 @@ export default function Weather(props) {
   
   function handleResponse(response){
     console.log(response.data);
-    setCurrentCity(response.data.name);
-    setTemperature(Math.round(response.data.main.temp));
-    setFeel(Math.round(response.data.main.feels_like));
-    setHumidity(response.data.main.humidity);
-    setWindSpeed(Math.round(response.data.wind.speed));
     
+    setWeatherData({
+      temperature: Math.round(response.data.main.temp),
+      feel: Math.round(response.data.main.feels_like),
+      humidity: response.data.main.humidity,
+      windSpeed: Math.round(response.data.wind.speed),
+      currentCity: response.data.name,
+    })
   }
 
 
@@ -40,7 +39,7 @@ export default function Weather(props) {
     <div className="Weather">
     <div className="row">
         <div className="col-6">
-      <h1 className="Current-City">{currentCity}</h1>
+      <h1 className="Current-City">{weatherData.currentCity}</h1>
           <h2 className="Current-Day-Time">Thursday</h2>
           <h2 className="Current-Condition">Sunny</h2>
           
@@ -48,21 +47,21 @@ export default function Weather(props) {
     <div className="col-6">
        <h3>
         <img
-          src="https://d29fhpw069ctt2.cloudfront.net/icon/image/49043/preview.svg"
-          alt="Sunny"
+          src={iconUrl}
+          alt={weatherData.description}
         />
-        {" "}{temperature}° <a href="/">F</a>|<a href="/">C</a>
+        {" "}{weatherData.temperature}° <a href="/">F</a>|<a href="/">C</a>
       </h3>
       <ul>
-        <li>Feels like: {feel}°</li>
-        <li>Humidity: {humidity}%</li>
-        <li>Wind Speed: {windSpeed} mph</li>
+        <li>Feels like: {weatherData.feel}°</li>
+        <li>Humidity: {weatherData.humidity}%</li>
+        <li>Wind Speed: {weatherData.windSpeed} mph</li>
       </ul>
     </div>
       <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-5">
-            <input id="Search-form" type="search" placeholder="Search a city" autoFocus="on" onChange={changeCity} />
+            <input id="Search-form" type="search" placeholder="Search a city" autoFocus="off" onChange={changeCity} />
           </div>
         
           <div className="col-7">
