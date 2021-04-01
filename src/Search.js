8a1733fa9ca3ee115591032e933e442275ff0a91
eslazyk10 <eslazy32@gmail.com>
2021-04-01 14:34:52 -0500

    imported axios, modified some formating and layout, started building search engine functions to call axios

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Search.css";
 
 export default function Search() {
+  let [city, setCity]= useState("")
+  const apiKey = "5b8bfd096caf5847f3506db76bfb75ad";
+  const unit= "metric"
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  function handleSubmit(event){
+    event.preventDefault()
+
+    axios.get(apiUrl).then(showWeather)
+  }
+
+  function changeCity(event){
+    event.preventDefault()
+    setCity(event.target.value)
+  }
+  
+  function showWeather(response){
+    console.log(response)
+  }
+
   return (
+    
     <div className="Search">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-5">
-            <input id="Search-form" type="search" placeholder="Search a city" />
+            <input id="Search-form" type="search" placeholder="Search a city" autoFocus="on" onChange={changeCity} />
           </div>
         
           <div className="col-7">
