8df2f50a632596f248233b894294568de11c3893
eslazyk10 <eslazy32@gmail.com>
2021-04-02 20:37:11 -0500

    utilized loops and conditional statements to create multiple forecast components for next several days

--- src/Forecast.js
+++ src/Forecast.js
@@ -18,9 +18,16 @@ export default function Forecast(props){
         return(
         <div className="Forecast">
             <div className="row">
-                <div className="col">
-                    <ForecastDay data={forecast[0]}/>
-                </div>
+                {forecast.map(function(dailyForecast, index){
+                    if (index < 6){
+                    return (
+                    <div className="col" key={index}>
+                        <ForecastDay data={dailyForecast}/>
+                    </div>
+                    );
+                    }
+                })}
+                
             </div>
         </div>
     );
